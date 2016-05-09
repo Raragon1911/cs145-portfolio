97dce0695c43a15f669244be87aeb6966124d562
ramos <hvaok3030@gmail.com>
2016-05-09 11:32:10 -0600

    changes

--- MyPortfolio.java
+++ MyPortfolio.java
@@ -27,6 +27,8 @@ class MyPortfolio {
      * the screen on one line.
      */	
 	static void printIntegerList(int[] values) {
+        for(int i=0;i<values.length;i++)
+            System.out.print(values[i]);
 
 	}
 
@@ -36,6 +38,8 @@ class MyPortfolio {
      * ORDER to the screen on one line.
      */	
 	static void printIntegerListReversed(int[] values) {
+        for(int i=values.length-1;i>0;i--)
+            System.out.print(values[i]);
 
 	}
 
@@ -44,7 +48,10 @@ class MyPortfolio {
      * and returns the "new" left_char.
       */	
 	static char swapLeftWithRight(char left_char, char right_char) {
-		return ' ';
+        char temp_char = left_char;
+        left_char = right_char;
+        right_char = temp_char;
+		return left_char;
 	}
 
     /**
@@ -53,7 +60,9 @@ class MyPortfolio {
      * containing the swapped version of the array passed in as the argument.
      */	
 	static int[] swapFirstWithLast(int[] values) {
-
+        int temp = values[0];
+       values[0] = values[ values.length-1];
+        values[values.length-1] = temp;
 		return values;
 	}
 
@@ -62,8 +71,13 @@ class MyPortfolio {
      * found in the list.
      */	
 	static int min(int[] values) {
-
-		return 0;
+        int min = values[0];
+        for(int i = 0; i < values.length; i++)
+            if(values[i]<min){
+                min = values[i];
+            }
+       
+		return min;
 	}
 
     /**
@@ -71,8 +85,13 @@ class MyPortfolio {
      * found in the list.
      */	
 	static int max(int[] values) {
+        int max = values[0];
+        for(int i = 0; i < values.length;i++)
+            if(values[i]>max){
+                max = values[i];
+            }
 
-		return 0;
+		return max;
 	}
 
     /**
@@ -80,8 +99,16 @@ class MyPortfolio {
      * values found in the list.
      */	
 	static int findAverage(int[] values) {
+		int sum = 0;
+		for( int i : values) {
+   			 sum += i;
+			}
+		int average = 9;
+		if(values.length > 0){
+			average = sum/values.length;
+		}
 
-		return 0;
+		return average;
 	}
 
     /**
